568d6640824248752bc774865c2f3fcfd1e0905b
subhashish-rumsan <subhashishjungshah@gmail.com>
2024-06-28 11:48:43 +0545

    Removed content script

--- src/content/ContentApp.tsx
+++ src/content/ContentApp.tsx
@@ -1,59 +1,59 @@
-import { Device } from "@twilio/voice-sdk";
-import React, { useRef } from "react";
-import useFetchToken from "../hooks";
+// import { Device } from "@twilio/voice-sdk";
+// import React, { useRef } from "react";
+// import useFetchToken from "../hooks";
 
-const ContentApp: React.FC = () => {
-  const requestMicrophoneAccess = () => {
-    chrome.runtime.sendMessage({ action: "requestMicrophone" }, (response) => {
-      console.log(response.status);
-    });
-  };
-  const { tokenData } = useFetchToken();
+// const ContentApp: React.FC = () => {
+//   const requestMicrophoneAccess = () => {
+//     chrome.runtime.sendMessage({ action: "requestMicrophone" }, (response) => {
+//       console.log(response.status);
+//     });
+//   };
+//   const { tokenData } = useFetchToken();
 
-  const device = useRef<Device | null>(null);
+//   const device = useRef<Device | null>(null);
 
-  const handleConferenceCall = async () => {
-    try {
-      if (!device.current) {
-        device.current = new Device(tokenData?.token, {
-          codecPreferences: ["opus", "pcmu"], // Adjust codec preferences as needed
-        });
+//   const handleConferenceCall = async () => {
+//     try {
+//       if (!device.current) {
+//         device.current = new Device(tokenData?.token, {
+//           codecPreferences: ["opus", "pcmu"], // Adjust codec preferences as needed
+//         });
 
-        device.current.register();
+//         device.current.register();
 
-        const params = {
-          To: "+14154493912",
-          callerId: "+14156875897",
-        };
+//         const params = {
+//           To: "+14154493912",
+//           callerId: "+14156875897",
+//         };
 
-        const callInstance = await device.current.connect({ params });
+//         const callInstance = await device.current.connect({ params });
 
-        callInstance.on("accept", () => {});
+//         callInstance.on("accept", () => {});
 
-        callInstance.on("ringing", () => {});
+//         callInstance.on("ringing", () => {});
 
-        callInstance.on("answered", () => {});
+//         callInstance.on("answered", () => {});
 
-        callInstance.on("connected", () => {});
+//         callInstance.on("connected", () => {});
 
-        callInstance.on("disconnect", () => {});
+//         callInstance.on("disconnect", () => {});
 
-        callInstance.on("cancel", () => {});
-      }
-    } catch (error) {
-      console.error("Error making call:", error);
-    }
-  };
+//         callInstance.on("cancel", () => {});
+//       }
+//     } catch (error) {
+//       console.error("Error making call:", error);
+//     }
+//   };
 
-  return (
-    <div>
-      <h1>Hello from Content App</h1>
-      <button onClick={requestMicrophoneAccess}>
-        Request Microphone Access
-      </button>
-      <button onClick={handleConferenceCall}>Make call</button>
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <h1>Hello from Content App</h1>
+//       <button onClick={requestMicrophoneAccess}>
+//         Request Microphone Access
+//       </button>
+//       <button onClick={handleConferenceCall}>Make call</button>
+//     </div>
+//   );
+// };
 
-export default ContentApp;
+// export default ContentApp;
--- src/content/content.tsx
+++ src/content/content.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./content.css";
-import ContentApp from "./ContentApp";
+// import ContentApp from "./ContentApp";
 
 const root = document.createElement("div");
 root.id = "crx-root";
@@ -10,30 +10,28 @@ document.body.appendChild(root);
 console.log("Hello world from content script");
 
 ReactDOM.createRoot(root).render(
-  <React.StrictMode>
-    <ContentApp />
-  </React.StrictMode>
+  <React.StrictMode>{/* <ContentApp /> */}</React.StrictMode>
 );
 
 // Listen for messages from the background script
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.action === "requestMicrophone") {
-    navigator.mediaDevices
-      .getUserMedia({ audio: true })
-      .then((stream) => {
-        console.log("Microphone access granted");
-        stream.getTracks().forEach(function (track) {
-          track.stop();
-        });
-        sendResponse({ status: "Microphone access granted" });
-      })
-      .catch((error) => {
-        console.error("Error requesting microphone permission", error);
-        sendResponse({
-          status: "Error requesting microphone permission",
-          error: error,
-        });
-      });
-    return true; // Keep the message channel open for sendResponse
-  }
-});
+// chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+//   if (request.action === "requestMicrophone") {
+//     navigator.mediaDevices
+//       .getUserMedia({ audio: true })
+//       .then((stream) => {
+//         console.log("Microphone access granted");
+//         stream.getTracks().forEach(function (track) {
+//           track.stop();
+//         });
+//         sendResponse({ status: "Microphone access granted" });
+//       })
+//       .catch((error) => {
+//         console.error("Error requesting microphone permission", error);
+//         sendResponse({
+//           status: "Error requesting microphone permission",
+//           error: error,
+//         });
+//       });
+//     return true; // Keep the message channel open for sendResponse
+//   }
+// });
